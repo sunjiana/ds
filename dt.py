3034a406002b995ae91bc58de94fe543ca0d0d7e
sunjiana <61388089+sunjiana@users.noreply.github.com>
2025-05-05 06:10:37 +0800

    Update dt.py

--- dt.py
+++ dt.py
@@ -126,7 +126,7 @@ results.sort(key=lambda x: channel_key(x[0]))
 
 result_counter = 8  # 每个频道需要的个数
 
-with open("ds.txt", 'w', encoding='utf-8') as file:
+with open("dt.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('河北频道,#genre#\n')
     for result in results:
