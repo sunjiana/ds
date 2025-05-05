0a7830caeb811cf79a713161b5c0dc4c0f420bb9
sunjiana <61388089+sunjiana@users.noreply.github.com>
2025-05-05 11:07:21 +0800

    Update da.py

--- da.py
+++ da.py
@@ -11,8 +11,7 @@ headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537
 
 urls = [
   
-# "https://www.shodan.io/search?query=ZHGXTV",
-  "https://www.shodan.io/search?query=ZHGXTV+org%3A%22CHINANET+HEILONGJIANG+PROVINCE+NETWORK%22",
+ "https://www.shodan.io/search?query=ZHGXTV",
    
 ]
 
